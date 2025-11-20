421fbd6bcffad5a2d7e45d90a2c7b1810a65fcf3
Lee Massey <lee.m.massey@gmail.com>
2025-11-19 18:05:29 -0800

    Add solution to exercise 14 - Fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let count = parseInt(n);
 
-};
+    let first = 1;
+    let second = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = first + second;
+        second = first;
+        first = current;
+    }
+
+    return first;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
