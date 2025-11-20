05b8b0debeef7a648fb2938d57430263a9ef79e0
Lee Massey <lee.m.massey@gmail.com>
2025-11-19 17:37:03 -0800

    Commit solution for exercise 13 - Palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+function palindromes(str) {
+    const alphaNumeric = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanedStr = str.toLowerCase().split('').filter(character => alphaNumeric.includes(character)).join('');
+
+    const reversedStr = cleanedStr.split('').reverse().join('');
+    
+    return reversedStr === cleanedStr;
 };
 
 // Do not edit below this line
